2453dd0dd3d2fe063db2d13575a6b68edd1acdfa
Md Shahinur Khan <64772153+shahinurdev@users.noreply.github.com>
2024-06-05 12:38:51 +0600

    add

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+app.use(cors());
+app.use(express.json());
+
+function createToken (user) {
+const token = jwt.sign({
+  email: user.email
+},
+'secret', 
+{ expiresIn: '7d' });
+return token;
+};
+function verifyToken (req, res, next) {
+  const authToken = req.headers.authorization.split(' ')[1];
+  const verifyToken = jwt.verify(authToken,"secret");
+  if(!verifyToken?.email){
+    return res.send("You are not authorized ")
+  }
+  req.user = verifyToken.email;
+  next()
+}
+
+
+
+
+const uri = "mongodb+srv://mdshahinurkhan6:qj9TLpRQ6IL3eT5m@cluster0.9ppsooh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const clothesDB = client.db("clothesDB");
+    const usersDB = client.db("usersDB");
+    const clotheCollection = clothesDB.collection("clothe_collection");
+    const usersCollection = usersDB.collection("usersCollection");
+
+    // cloth routes
+    app.post("/clothes", async (req, res) => {
+      const clotheData = req.body;
+      const result = await clotheCollection.insertOne(clotheData);
+      res.send(result);
+    });
+
+    app.get("/clothes", async (req, res) => {
+      const clotheData = clotheCollection.find();
+      const result = await clotheData.toArray();
+      res.send(result);
+    });
+
+    app.get("/clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const clotheData = await clotheCollection.findOne({ _id: new ObjectId(id) });
+      res.send(clotheData);
+    });
+
+    app.patch("/clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const result = await clotheCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
+      res.send(result);
+      console.log(result);
+    });
+
+    app.delete("/clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await clotheCollection.deleteOne(
+        { _id: new ObjectId(id) }
+      );
+      res.send(result);
+      console.log(result);
+    });
+
+    // user routes
+    app.post("/user",verifyToken , async (req, res) => {
+      const user = req.body;
+      const token = createToken(user);
+      const isUserExist = await usersCollection.findOne({ email: user?.email });
+      if (isUserExist?._id) {
+        return res.send({
+          status: "welcome login ",
+          message: "Login successfully",
+          token
+        });
+      }
+    await usersCollection.insertOne(user);
+      res.send({token});
+    });
+
+    app.get("/user/get/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
+    app.patch("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+      const result = await usersCollection.updateOne(
+        { email },
+        { $set: userData },
+        {upsert: true },
+      );
+      res.send(result);
+    });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally { 
+    // Optionally close the client connection
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.status(200).send("welcome to server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
